38a6cf0d50b765440de85f753979af357858caf3
Gaurav <grv0896@gmail.com>
2023-10-30 00:00:44 +0530

    Added basic functionality to player

--- player/index.js
+++ player/index.js
@@ -4,10 +4,38 @@ document.addEventListener("DOMContentLoaded", function() {
 	const video = document.getElementById("video");
 	const videoControls = document.getElementById("video-controls");
 	video.controls = false;
-	videoControls.style.display = "block";
+	//videoControls.style.display = "block";
 
 	if (supportsVideo) {
-		//do stuff here;
+		const playPause = document.getElementById('playpause');
+		const stop = document.getElementById('stop');
+		const toggleMute = document.getElementById('mute');
+		const progress = document.getElementById("progress");
+		const progressBar = document.getElementById('progress-bar');
+
+		playPause.addEventListener("click", (e) => {
+			if (video.paused || video.ended) {
+				video.play();
+			} else {
+				video.pause();
+			}
+		});
+
+		stop.addEventListener("click", (e) => {
+			video.pause();
+			video.currentTime = 0;
+		});
+
+		toggleMute.addEventListener("click", (e) => {
+			video.muted = !video.muted;
+		});
+
+		video.addEventListener("timeupdate", () => {
+			if (!progress.getAttribute('max')) progress.setAttribute('max', video.duration);
+			progress.value = video.currentTime;
+			progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+		});
+
 	}
 });
 
