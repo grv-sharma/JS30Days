c0dbfffb5d71d9ba11b7e94a358e0323ca79efcf
Gaurav <grv0896@gmail.com>
2023-11-01 03:13:32 +0530

    Basic custom player done

--- player/index.js
+++ player/index.js
@@ -12,6 +12,17 @@ document.addEventListener("DOMContentLoaded", function() {
 		const toggleMute = document.getElementById('mute');
 		const progress = document.getElementById("progress");
 		const progressBar = document.getElementById('progress-bar');
+		const volinc = document.getElementById('volinc');
+		const voldec = document.getElementById('voldec');
+		const fullScr = document.getElementById('fs');
+
+		video.addEventListener("click", () => {
+			if (video.paused) {
+				video.play();
+			} else {
+				video.pause();
+			}
+		});
 
 		playPause.addEventListener("click", (e) => {
 			if (video.paused || video.ended) {
@@ -36,6 +47,41 @@ document.addEventListener("DOMContentLoaded", function() {
 			progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
 		});
 
+		volinc.addEventListener("click", () => {
+			if (video.volume == 1) {
+				return;
+			} else {
+				video.volume += 0.1;
+			}
+		});
+
+		voldec.addEventListener("click", () => {
+			if (video.volume <= 0.1 ) {
+				return;
+			} else {
+				video.volume -= 0.1;
+			}
+		});
+		
+		progress.addEventListener("click", (e) => {
+			const pos = 
+				(e.pageX - progress.offsetLeft - progress.offsetParent.offsetLeft) / 
+				progress.offsetWidth;
+			video.currentTime = pos * video.duration;
+		});
+
+		function handleFullScreen() {
+			if (document.fullscreenElement !== null) {
+				document.exitFullscreen();
+				//setFullscreenData(false);
+			} else {
+				video.requestFullscreen();
+				//setFullscreenData(true);
+			}
+		}
+
+		
+		fullScr.addEventListener("click", handleFullScreen);
 	}
 });
 
