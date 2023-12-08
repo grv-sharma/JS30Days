098be1b925f0da4ee1a8d5994ca68c57db0ead51
Gaurav <grv0896@gmail.com>
2023-12-08 23:39:39 +0530

    local-tapas all functionality done

--- local-tapas/index.js
+++ local-tapas/index.js
@@ -2,13 +2,47 @@ document.addEventListener("DOMContentLoaded", function() {
     unOList = document.getElementById("ulist");
     deText = document.getElementById("defaultText");
     line = document.getElementById("seperator");
+    clearBtn = document.getElementById("clr");
     saveItems = [];
     let count = 0;
+
+    //clear
+    clearBtn.addEventListener("click", function() {
+        localStorage.clear();
+        location.reload();
+    });
+
+    
+    function statusSwitcher() {
+        inps = document.querySelectorAll('input');
+        console.log("switcher executed");
+        //set data-status
+        inps.forEach((el) => {
+            el.addEventListener("click", function() {
+                wholeList = JSON.parse(localStorage.getItem("taco-list"));
+                console.log(wholeList);
+                for (let i = 0; i < wholeList.length; i++) {
+                    if (el.dataset.index == wholeList[i][3]) {
+                        console.log('if-entered');
+                        console.log(el);
+                        wholeList[i][2] = !wholeList[i][2];
+                    }
+                }
+                localStorage.clear();
+                localStorage.setItem("taco-list", JSON.stringify(wholeList));
+                unOList.innerHTML = '';
+                restoreItems('taco-list');
+            });
+        });
+    }
+    
 
     document.addEventListener("submit", function(e) {
         const list = document.createElement("input");
         list.type = 'checkbox';
         list.id = `box${count}`;
+        list.dataset.status = 'false';
+        list.dataset.index = count;
 
         const label = document.createElement("label");
         label.htmlFor = `box${count}`;
@@ -35,15 +69,19 @@ document.addEventListener("DOMContentLoaded", function() {
         const divider = document.createElement("hr");
         unOList.appendChild(divider);
         count++;
-
+    
+        
         // push box-id and item-text into array
-        saveItems.push([`box${count}`, `${options.value}`]);
+        //saveItems.push([`box${count}`, `${options.value}`]);
+        //push with data-status
+        saveItems.push([`box${count}`, `${options.value}`, `${list.dataset.status}`, `${list.dataset.index}`]);
         //console.log(saveItems);
         form.reset();
 
         //localStorage.setItem("taco-list", JSON.stringify(saveItems));
         //appendToStorage("taco-list", `box${count}`, `${options.value}`);
         persistentStorage("taco-list", saveItems);
+        statusSwitcher();
     });
 
     function restoreItems(key) {
@@ -66,6 +104,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 const divider = document.createElement('hr');
                 chk.type = 'checkbox';
                 chk.id = savedListItemsParsed[i][0];
+                chk.dataset.status = savedListItemsParsed[i][2];
+                chk.dataset.index = savedListItemsParsed[i][3];
+                // check previously checked items
+                if (chk.dataset.status === 'true') {
+                    console.log('true detected');
+                    chk.checked = true;
+                }
 
                 const lbl = document.createElement('label');
                 lbl.htmlFor = savedListItemsParsed[i][0];
@@ -78,6 +123,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 console.log(count);
             }
         }   
+        statusSwitcher();
     }
 
 
@@ -95,6 +141,5 @@ document.addEventListener("DOMContentLoaded", function() {
         }  
     }
 
-    restoreItems();
-
+    restoreItems(); 
 })
